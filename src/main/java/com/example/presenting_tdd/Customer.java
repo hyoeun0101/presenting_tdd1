d4eac9be63ad81d3d419a0af903616c43b3b856c
khe <kimhyoeun0101@gmail.com>
2025-08-21 14:14:38 +0900

    make 'childrenMovie 3일 대여' test pass

--- src/main/java/com/example/presenting_tdd/Customer.java
+++ src/main/java/com/example/presenting_tdd/Customer.java
@@ -1,4 +1,20 @@
 package com.example.presenting_tdd;
 
+enum MovieType {
+    CHILDREN,
+    REGULAR,
+    NEW_RELEASE,
+}
 public class Customer {
+    public void rentMovie(String title, MovieType movieType, int daysRented) {
+
+    }
+
+    public Integer getRentalPoints() {
+        return 1;
+    }
+
+    public Double getCharge() {
+        return 1.5;
+    }
 }
--- src/test/java/com/example/presenting_tdd/PresentingTddApplicationTests.java
+++ src/test/java/com/example/presenting_tdd/PresentingTddApplicationTests.java
@@ -3,6 +3,9 @@ package com.example.presenting_tdd;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.completionStage;
+
 //@SpringBootTest
 class PresentingTddApplicationTests {
 
@@ -11,4 +14,14 @@ class PresentingTddApplicationTests {
 		new Customer();
 	}
 
+	@Test
+	void childrensMovie_3일_대여한_경우() {
+		//Arrange
+		Customer customer = new Customer();
+		//Act
+		customer.rentMovie("title", MovieType.CHILDREN, 3);
+		//Assert
+		assertThat(customer.getRentalPoints()).isEqualTo(1);
+		assertThat(customer.getCharge()).isEqualTo(1.5);
+	}
 }
