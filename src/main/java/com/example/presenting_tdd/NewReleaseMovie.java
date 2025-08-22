9ff83af2eed8ea5f103a96945bdb3e1b10533d37
khe <kimhyoeun0101@gmail.com>
2025-08-22 10:02:12 +0900

    remove unused code with coverage

--- src/main/java/com/example/presenting_tdd/NewReleaseMovie.java
+++ src/main/java/com/example/presenting_tdd/NewReleaseMovie.java
@@ -7,7 +7,7 @@ public class NewReleaseMovie extends Movie {
 
     @Override
     public Integer getRentalPoints(int daysRented) {
-        if (movieType == MovieType.NEW_RELEASE && daysRented > 1) {
+        if (daysRented > 1) {
             return 2;
         }
         return 1;
@@ -15,20 +15,6 @@ public class NewReleaseMovie extends Movie {
 
     @Override
     public Double getCharge(int daysRented) {
-        double charge = 0;
-        if (movieType == MovieType.CHILDREN) {
-            charge = 1.5;
-            if (daysRented > 3) {
-                charge += (daysRented - 3) * 1.5;
-            }
-        } else if (movieType == MovieType.REGULAR) {
-            charge = 2;
-            if (daysRented > 2) {
-                charge += (daysRented - 2) * 1.5;
-            }
-        } else if (movieType == MovieType.NEW_RELEASE) {
-            charge += daysRented * 3;
-        }
-        return charge;
+        return (double) (daysRented * 3);
     }
 }
--- src/main/java/com/example/presenting_tdd/RegularMovie.java
+++ src/main/java/com/example/presenting_tdd/RegularMovie.java
@@ -7,27 +7,14 @@ public class RegularMovie extends Movie {
 
     @Override
     public Integer getRentalPoints(int daysRented) {
-        if (movieType == MovieType.NEW_RELEASE && daysRented > 1) {
-            return 2;
-        }
         return 1;
     }
 
     @Override
     public Double getCharge(int daysRented) {
-        double charge = 0;
-        if (movieType == MovieType.CHILDREN) {
-            charge = 1.5;
-            if (daysRented > 3) {
-                charge += (daysRented - 3) * 1.5;
-            }
-        } else if (movieType == MovieType.REGULAR) {
-            charge = 2;
-            if (daysRented > 2) {
-                charge += (daysRented - 2) * 1.5;
-            }
-        } else if (movieType == MovieType.NEW_RELEASE) {
-            charge += daysRented * 3;
+        double charge = 2;
+        if (daysRented > 2) {
+            charge += (daysRented - 2) * 1.5;
         }
         return charge;
     }
